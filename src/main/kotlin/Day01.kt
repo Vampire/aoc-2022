fae9c753ab635faada853f3290380cd1386f1f84
Björn Kautler <Bjoern@Kautler.net>
2022-12-02 02:06:27 +0100

    Solve Day 01

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,16 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun calorySums(caloryList: List<String>): List<Int> {
+        var calories = 0
+        val calorySums = caloryList.mapNotNull {
+            if (it.isEmpty()) {
+                return@mapNotNull calories.also { calories = 0 }
+            } else {
+                calories += it.toInt()
+                return@mapNotNull null
+            }
+        }
+        return calorySums
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = calorySums(input).max()
+
+    fun part2(input: List<String>) = calorySums(input).sortedDescending().take(3).sum()
 
     val testInput = readStrings("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24_000)
 
     val input = readStrings("Day01")
     println(part1(input))
+
+    check(part2(testInput) == 45_000)
     println(part2(input))
 }
